52514db8decbc5edfa7d9fab7ef3a2063a5018e2
soviea <150814343+soviea@users.noreply.github.com>
2023-11-14 21:57:39 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = "http://cat.lovestoblog.com/graphql."
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.toprevenuegate.com/tth2iuh6y6?key=dfc3e1ad2a575eafe0ca205c9baa891e` 
+					`http://cat.lovestoblog.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
